baf27dbad53a8ba929d9d3019d956af9f0e6f989
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-09-14 19:25:03 +0300

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,24 @@ let outputEl = document.getElementById("output")
 let btnCEl = document.getElementById("btnC")
 let btn_clearEl = document.getElementById("btn_clear")
 let btn_plusEl = document.getElementById ("btn_plus")
+let btn1El = document.getElementById ("btn1")
+let btn2El = document.getElementById ("btn2")
+let btn3El = document.getElementById ("btn3")
+let minusEl = document.getElementById ("minus")
+let btn4El = document.getElementById ("btn4")
+let btn5El = document.getElementById ("btn5")
+let btn6El = document.getElementById ("btn6")
+let multipleEl = document.getElementById ("multiple")
+let btn7El = document.getElementById ("btn7")
+let btn8El = document.getElementById ("btn8")
+let btn9El = document.getElementById ("btn9")
+let equalEl = document.getElementById ("equal")
+let btn0El = document.getElementById ("btn0")
+let powerEl = document.getElementById ("power")
+let divisionEl = document.getElementById ("division")
 document.title = "calculator"
+
+btnCEl.addEventListener("click",function(){
+   
+    
+})
