607a08250d01c79f810f1c11439734cd9da658ae
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-09-14 18:52:59 +0300

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
+let outputEl = document.getElementById("output")
+let btnCEl = document.getElementById("btnC")
+let btn_clearEl = document.getElementById("btn_clear")
+let btn_plusEl = document.getElementById ("btn_plus")
 document.title = "calculator"
